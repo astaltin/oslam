3a52d225c1492e824f97e3c3574697381b3ca59b
Edward Gulmayo <astaltin@gmail.com>
2024-10-24 16:48:48 +0800

    feat: add landing page skeleton with navbar and sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,93 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <>
+      <header>
+        <section>
+          <div>
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/oslam-logo.png"
+              width={48}
+              height={48}
+              alt="oslam_logo"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+            <span>OSLAM</span>
+          </div>
+
+          <nav>
+            <Link href="#services">Services</Link>
+            <Link href="#mission">Mission</Link>
+            <Link href="#about">About</Link>
+          </nav>
+        </section>
+
+        <Image
+          src="/oslam-panorama-img.jpg"
+          width="100"
+          height="100"
+          alt="oslam_hero"
+        />
+      </header>
+
+      <main>
+        <section id="services">
+          <h2>Services</h2>
+          <ul>
+            <li>Internal Medicine</li>
+            <li>Ob Gynecology / Perinatology</li>
+            <li>Pediatrics</li>
+            <li>General Medicine</li>
+            <li>Family Medicine</li>
+          </ul>
+        </section>
+
+        <section id="mission">
+          <h2>Our Mission</h2>
+          <p>
+            <em>OSLAM</em> is commited to provide compassionate, affordable and
+            effective patient-centered medical care that promotes health and
+            well-being to the community.
+          </p>
+        </section>
+
+        <section id="about">
+          <h2>About</h2>
+          <p>
+            <em>Ospital ng Lungsod Agham ng Muñoz</em> is a premier healthcare
+            facility located in the <em>Science City of Muñoz, Nueva Ecija</em>.
+            We are committed to providing exceptional medical services that
+            cater to the unique needs of our community. With a focus on
+            delivering patient-centered, quality healthcare, we strive to
+            improve the health and well-being of all those we serve.
+          </p>
+          <p>
+            Our team of dedicated professionals is here to ensure that every
+            patient receives personalized care in a welcoming and compassionate
+            environment. We work tirelessly to offer advanced medical treatments
+            while upholding the highest standards of care.
+          </p>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+
+      <footer>
+        <section>
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/oslam-logo.png"
+            width={72}
+            height={72}
+            alt="oslam_logo"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+          <nav>
+            <Link href="#services">Services</Link>
+            <Link href="#mission">Mission</Link>
+            <Link href="#about">About</Link>
+          </nav>
+        </section>
+
+        <p>OSLAM • Copyright © 2024</p>
       </footer>
-    </div>
+    </>
   );
 }
