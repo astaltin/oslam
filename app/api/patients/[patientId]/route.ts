74e4fd1971f96093c9f8450b7708ff7d47a35281
Edward Gulmayo <astaltin@gmail.com>
2024-11-15 22:53:59 +0800

    feat(patients): add delete endpoint for patient records

--- app/api/patients/[patientId]/route.ts
+++ app/api/patients/[patientId]/route.ts
@@ -1,12 +1,11 @@
 import { prisma } from "@/lib/prisma";
 import { isPrismaError } from "@/util/isPrismaError";
 
+type Params = { params: Promise<{ patientId: string }> };
+
 const isDevelopment = process.env.NODE_ENV === "development";
 
-export async function PATCH(
-  request: Request,
-  { params }: { params: Promise<{ patientId: string }> },
-) {
+export async function PATCH(request: Request, { params }: Params) {
   const patientId = Number((await params).patientId);
   const data = await request.json();
 
@@ -37,5 +36,26 @@ export async function PATCH(
     }
 
     throw error;
+  }
+}
+
+export async function DELETE(request: Request, { params }: Params) {
+  const patientId = Number((await params).patientId);
+
+  try {
+    await prisma.patient.delete({ where: { patientId } });
+
+    return new Response(null, { status: 204 });
+  } catch (error) {
+    if (isPrismaError(error)) {
+      if (isDevelopment) {
+        console.error(
+          "A prisma error occured while trying to delete a patient data:",
+          error,
+        );
+      }
+
+      return new Response(null, { status: 500 });
+    }
   }
 }
